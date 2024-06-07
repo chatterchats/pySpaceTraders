f5801afc7fb6802d7d62987ad224b72ad2c3407c
Chats <chats@irby.us>
2024-06-06 22:02:39 -0400

    Implement Contract and Faction Endpoints
    Adjust DocStrings and make_request URLS

--- pySpaceTraders/api_client.py
+++ pySpaceTraders/api_client.py
@@ -1,19 +1,23 @@
 import json
 import os.path
 from typing import Optional
-from pySpaceTraders.utils import make_request
-from pySpaceTraders.models.factions import Factions
 
 # Return Models
 from pySpaceTraders.models.agent import AgentResponse, AgentListResponse
+from pySpaceTraders.models.contract import ContractResponse, ContractsListResponse, ContractAcceptResponse, \
+    DeliverCargoResponse, \
+    ContractFulfillResponse
+from pySpaceTraders.models.factions import Factions, FactionResponse, FactionListResponse
+from pySpaceTraders.utils import make_request
 
 
 class SpaceTraders:
     def __init__(self):
         self.token: str = ""
 
-    def status(self):
-        response = make_request("GET", "")
+    @staticmethod
+    def status():
+        response = make_request("GET", "/")
         return response
 
     def register(
@@ -32,13 +36,7 @@ class SpaceTraders:
             - The symbol of the faction. >= 1 characters
         - *email: Optional[str] (Defaults Blank)
             - Your email address. This is used if you reserved your call sign between resets.
-
-        ### Returns
-        - Dict
-            - Contains JWT Token to authenticate with other endpoints.
         """
-        method = "POST"
-        endpoint = "register"
         faction = faction.upper()
         payload = {"symbol": callsign, "faction": faction}
         if email:
@@ -49,27 +47,21 @@ class SpaceTraders:
             response = json.load(f)
             f.close()
         else:
-            response = make_request(method, endpoint, params=payload).json()
+            response = make_request("POST", "/register", params=payload).json()
+            token = {"data": {"token": response["data"]["token"]}}
             with open("token.json", "w", encoding="utf-8") as f:
-                json.dump(response, f, indent=4, ensure_ascii=False)
+                json.dump(token, f, indent=4, ensure_ascii=False)
 
         self.token = response["data"]["token"]
         return response
 
+    # Agent Endpoints #
     def my_agent(self) -> AgentResponse:
         """Fetch single agent details.
         ### Parameters
         - None
-        ### Returns
-        - Dict
-            - accountId: Optional[str] | Only if own agent.
-            - symbol: str
-            - headquarters: str
-            - credits: int
-            - startingFaction: str
-            - shipCount: int
         """
-        return make_request("GET", "my/agent", self.token).json()
+        return make_request("GET", "/my/agent", self.token).json()
 
     def list_agents(self, limit: int = 10, page: int = 1) -> AgentListResponse:
         """Fetch single agent details.
@@ -80,15 +72,6 @@ class SpaceTraders:
         - page: int (Defaults 1)
             - What entry offset to request
             - >= 1
-        ### Returns
-        - List of Dicts
-            - accountId: Optional[str] | Only if own agent.
-            - symbol: str
-            - headquarters: str
-            - credits: int
-            - startingFaction: str
-            - shipCount: int
-        - Dict[str, int]
         """
         # token optional for get_agent
         return make_request(
@@ -112,3 +95,55 @@ class SpaceTraders:
         """
         # token optional for get_agent
         return make_request("GET", f"/agents/{symbol}", self.token).json()
+
+    # Contracts Endpoints #
+    def list_contracts(self, limit: int = 10, page: int = 1) -> ContractsListResponse:
+        return make_request(
+            "GET", f"/my/contracts?limit={limit}&page={page}", self.token
+        ).json()
+
+    def get_contract(self, contract_id: str) -> ContractResponse:
+        return make_request(
+            "GET", f"/my/contracts/{contract_id}", self.token
+        ).json()
+
+    def accept_contract(self, contract_id: str) -> ContractAcceptResponse:
+        return make_request(
+            "POST", f"/my/contracts/{contract_id}/accept", self.token
+        ).json()
+
+    def deliver_contract_cargo(self, contract_id: str, ship_symbol: str, trade_symbol: str, units: int) -> DeliverCargoResponse:
+        payload = {
+            "shipSymbol": ship_symbol,
+            "tradeSymbol": trade_symbol,
+            "units": units,
+        }
+        return make_request(
+            "POST", f"/my/contracts/{contract_id}/deliver", self.token, params=payload
+        ).json()
+
+    def fulfill_contract(self, contract_id: str) -> ContractFulfillResponse:
+        return make_request(
+            "POST", f"/my/contracts/{contract_id}/fulfill", self.token
+        ).json()
+
+    # Faction Endpoints #
+
+    def list_factions(self, limit: int = 10, page: int = 1) -> FactionListResponse:
+        """Fetch single agent details.
+                ### Parameters
+                - limit: int (Defaults 10)
+                    - How many entries to return per page
+                    - >= 1 and <= 20
+                - page: int (Defaults 1)
+                    - What entry offset to request
+                    - >= 1
+                    """
+        return make_request(
+            "GET", f"/factions?limit={limit}&page={page}", self.token
+        ).json()
+
+    def get_faction(self, faction_symbol: str) -> FactionResponse:
+        return make_request(
+            "GET", f"/factions/{faction_symbol}", self.token
+        ).json()
