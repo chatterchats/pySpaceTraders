f69b16fa50fe256137406faabc5e451834277fec
Chats <chats@irby.us>
2024-06-11 03:49:45 -0400

    Add typing to constants.py

--- pySpaceTraders/constants.py
+++ pySpaceTraders/constants.py
@@ -1,4 +1,6 @@
 __version__ = "0.0.1"
 
-V2_STARTRADERS_URL = "https://api.spacetraders.io/v2"
-REQUEST_TYPES = ["GET", "POST", "PATCH"]
+from typing import List
+
+V2_STARTRADERS_URL: str = "https://api.spacetraders.io/v2"
+REQUEST_TYPES: List[str] = ["GET", "POST", "PATCH"]
