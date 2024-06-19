ea41ed6dfdc2c4626e5cd7df9de5da50e9128059
Chats <chats@irby.us>
2024-06-19 18:46:09 -0400

    Add testing URL

--- pySpaceTraders/constants.py
+++ pySpaceTraders/constants.py
@@ -2,7 +2,6 @@ __version__ = "0.5.0"
 
 from typing import List
 
-V2_STARTRADERS_URL: str = (
-    "https://stoplight.io/mocks/spacetraders/spacetraders/96627693"  # "https://api.spacetraders.io/v2"
-)
+V2_STARTRADERS_URL: str = "https://api.spacetraders.io/v2"
+V2_STOPLIGHT_URL = "https://stoplight.io/mocks/spacetraders/spacetraders/96627693"
 REQUEST_TYPES: List[str] = ["GET", "POST", "PATCH"]
